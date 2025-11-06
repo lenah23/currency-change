8cb1d6b538d23b7cb848a789e5187af7729c8570
lenah23 <lenulstep@gmail.com>
2025-11-06 12:19:01 +0400

    results static block

--- src/components/conversionResults/conversionResults.tsx
+++ src/components/conversionResults/conversionResults.tsx
@@ -1,7 +1,29 @@
 import styles from "./conversionResults.module.scss";
 
 const ConversionResults: React.FC = () => {
-  return <div>ConversionResults</div>;
+  return (
+    <div className={styles["conversation-result__block"]}>
+      <h2 className={styles["title"]}>Conversion result</h2>
+      <div className={styles["result-block"]}>
+        <div className={styles["result"]}>€0.92</div>
+        <div className={styles["from-value"]}>1 USD = </div>
+      </div>
+      <div className={styles["exchange-info"]}>
+        <div className={styles["exchange-pair"]}>
+          <span className={styles["label"]}>Exchange Rate</span>
+          <span className={styles["value"]}>1 USD = 0.920000 EUR</span>
+        </div>
+        <div className={styles["exchange-pair"]}>
+          <span className={styles["label"]}>Inverse Rate</span>
+          <span className={styles["value"]}>1 EUR = 1.086957 USD</span>
+        </div>
+      </div>
+      <div className={styles["additional-info"]}>
+        Rates are for informational purposes only and may not reflect real-time
+        market rates
+      </div>
+    </div>
+  );
 };
 
 export default ConversionResults;
