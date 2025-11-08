4c80a65180c2886b1ae28a31989cb1b0b86eb18c
lenah23 <lenulstep@gmail.com>
2025-11-08 20:31:30 +0400

    added loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import "./App.scss";
 import { ConverterMainContainer } from "./components";
+import { Suspense } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 function App() {
   return (
     <div className="App">
-      <ConverterMainContainer />
+      <Suspense
+        fallback={
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              minHeight: "100vh",
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        }
+      >
+        <ConverterMainContainer />
+      </Suspense>
     </div>
   );
 }
